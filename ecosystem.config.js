64fddb9e6c6cbcc34a3c449a0ef92d2b88e07705
munier-ie <238691132+munier-ie@users.noreply.github.com>
2025-12-31 05:35:07 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "ufriends-app",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+        PORT: 5070,
+      },
+      instances: 1,
+      exec_mode: "cluster",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      merge_logs: true,
+    },
+  ],
+};
